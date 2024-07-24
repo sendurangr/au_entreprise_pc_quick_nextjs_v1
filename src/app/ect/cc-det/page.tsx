2e6d5886b3b8f89f4cd4e189769276c7fb10c4c2
Senduran <senduran40@gmail.com>
2024-07-24 08:45:46 +0530

    Components Added

--- src/app/ect/cc-det/page.tsx
+++ src/app/ect/cc-det/page.tsx
@@ -1,4 +1,12 @@
 "use client";
+import * as React from "react"
+
+import {Button} from "@/components/ui/button"
+import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle,} from "@/components/ui/card"
+import {Input} from "@/components/ui/input"
+import {Label} from "@/components/ui/label"
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue,} from "@/components/ui/select"
+
 export default function Home() {
 
     // post method to /api/pass-cc
@@ -22,9 +30,62 @@ export default function Home() {
         );
     }
 
+    /*
+    <button onClick={() => postCC()}
+                        className={'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'}>
+                    Send CC
+                </button>
+    * */
+
     return (
-        <h1>
-            <button onClick={() => postCC()}>Click me</button>
-        </h1>
+        <div className="h-screen flex items-center">
+            <div className={"flex w-2/3 mx-auto"}>
+                <div className={"w-1/2"}>
+                    <div>
+                        <h2>
+                            Entreprise Corporate Traveller
+                        </h2>
+                        <h6>Secured Credit Card Information Portal</h6>
+                    </div>
+                </div>
+                <div className={"w-1/2"}>
+                    <Card className="w-[350px]">
+                        <CardHeader>
+                            <CardTitle>Create project</CardTitle>
+                            <CardDescription>Deploy your new project in one-click.</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <form>
+                                <div className="grid w-full items-center gap-4">
+                                    <div className="flex flex-col space-y-1.5">
+                                        <Label htmlFor="name">Name</Label>
+                                        <Input id="name" placeholder="Name of your project"/>
+                                    </div>
+                                    <div className="flex flex-col space-y-1.5">
+                                        <Label htmlFor="framework">Framework</Label>
+                                        <Select>
+                                            <SelectTrigger id="framework">
+                                                <SelectValue placeholder="Select"/>
+                                            </SelectTrigger>
+                                            <SelectContent position="popper">
+                                                <SelectItem value="next">Next.js</SelectItem>
+                                                <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                                                <SelectItem value="astro">Astro</SelectItem>
+                                                <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                                            </SelectContent>
+                                        </Select>
+                                    </div>
+                                </div>
+                            </form>
+                        </CardContent>
+                        <CardFooter className="flex justify-between">
+                            <Button variant="outline">Cancel</Button>
+                            <Button>Deploy</Button>
+                        </CardFooter>
+                    </Card>
+                </div>
+            </div>
+        </div>
+
     );
 }
